deb9cebb6e67a49f0a5fbf97c3e35f60effd34e7
Manish Goud <76445347+MANISHGOUD96@users.noreply.github.com>
2022-09-15 23:18:57 +0530

    Read Only Variable

---
+++ Read Only Variable (Static).cs
@@ -0,0 +1,19 @@
+﻿
+//.............Read Only Variable...........
+//.........Progarm-2 (static variable)......
+
+
+using System;
+class Test
+{
+    readonly static int z;
+    static Test()
+    {
+        z = 200;
+    }
+    static void Main()
+    {
+        Console.WriteLine(z);
+        Console.ReadKey();
+    }
+}
---
+++ Read Only Variable (non-Static).cs
@@ -0,0 +1,21 @@
+﻿
+//.............Read Only Variable.............
+//.......Progarm-1 (non static variable)......
+
+using System;
+
+class Show
+{
+    readonly  int x;
+    Show(int x)
+    {
+        this.x = x;
+    }
+    static void Main()
+    {
+        Show s = new Show(100);
+        Console.WriteLine(s.x);
+        Console.ReadKey();
+    }
+}
+
