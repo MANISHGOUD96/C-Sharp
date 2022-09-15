a0b54486e0ea2dc229aa7b114792931a9d4f2fe6
Manish Goud <76445347+MANISHGOUD96@users.noreply.github.com>
2022-09-15 23:12:37 +0530

    Anonymous Method

---
+++ Anonymous Method.cs
@@ -0,0 +1,22 @@
+﻿/*
+//..............Anonymous Method..................
+
+using System;
+
+class Test
+{
+    public void Show()
+    {
+        Console.WriteLine("Hi GoodMorning");
+    }
+  static void Main()
+    {
+        // Create object without using reference variable.
+        // So, this method is called as Anonymous Method.
+
+       new Test().Show();
+        Console.ReadKey();
+    }
+}
+
+*/
